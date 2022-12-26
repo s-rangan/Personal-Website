89eeaea09b1a41ff89b71eac5a0ddbe0abd2faf7
s-rangan <Sriram@Srirams-MacBook-Pro.local>
2022-12-26 17:55:21 -0500

    add untracked pictures, resume, and sourcecode view

---
+++ resources/views/components/home/source-code.blade.php
@@ -0,0 +1,133 @@
+<!-- ====== Call To Action Section Start -->
+<section>
+    <div class="bg-primary relative z-10 overflow-hidden py-12 px-8 md:p-[70px]">
+        <div class="container mx-auto">
+            <div class="-mx-4 flex flex-wrap items-center">
+                <div class="w-full px-4 lg:w-1/2">
+                    <span class="mb-2 text-base font-semibold text-white">
+                        Made with Laravel, Alpine.js, and Tailwind.css
+                    </span>
+                    <h2
+                        class="mb-6 text-3xl font-bold leading-tight text-white sm:mb-8 sm:text-[38px] lg:mb-0"
+                    >
+                        View the source code <br class="xs:block hidden" />
+                        and documentation.
+                    </h2>
+                </div>
+                <div class="w-full px-4 lg:w-1/2">
+                    <div class="flex flex-wrap lg:justify-end">
+                        <x-button-link
+                            href="https://github.com/s-rangan/Personal-Website"
+                            target="_blank"
+                            variant="dark" class="mr-3 my-1 font-bold" target="_blank"
+                        >
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" width="22px" height="22px" class="fill-current inline-block mb-1">
+                            <path 
+                                d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+                            />
+                        </svg>
+                            Source Code
+                        </x-button-link>
+                        <x-button-link
+                            href="https://laravel.com/docs/9.x"
+                            target="_blank"
+                            class="mr-3 my-1 bg-[#EA4532] font-bold hover:bg-opacity-80" target="_blank"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22px" height="22px" class="fill-current inline-block mb-1" viewBox="0 0 512 512">
+                                <path d="M504.4,115.83a5.72,5.72,0,0,0-.28-.68,8.52,8.52,0,0,0-.53-1.25,6,6,0,0,0-.54-.71,9.36,9.36,0,0,0-.72-.94c-.23-.22-.52-.4-.77-.6a8.84,8.84,0,0,0-.9-.68L404.4,55.55a8,8,0,0,0-8,0L300.12,111h0a8.07,8.07,0,0,0-.88.69,7.68,7.68,0,0,0-.78.6,8.23,8.23,0,0,0-.72.93c-.17.24-.39.45-.54.71a9.7,9.7,0,0,0-.52,1.25c-.08.23-.21.44-.28.68a8.08,8.08,0,0,0-.28,2.08V223.18l-80.22,46.19V63.44a7.8,7.8,0,0,0-.28-2.09c-.06-.24-.2-.45-.28-.68a8.35,8.35,0,0,0-.52-1.24c-.14-.26-.37-.47-.54-.72a9.36,9.36,0,0,0-.72-.94,9.46,9.46,0,0,0-.78-.6,9.8,9.8,0,0,0-.88-.68h0L115.61,1.07a8,8,0,0,0-8,0L11.34,56.49h0a6.52,6.52,0,0,0-.88.69,7.81,7.81,0,0,0-.79.6,8.15,8.15,0,0,0-.71.93c-.18.25-.4.46-.55.72a7.88,7.88,0,0,0-.51,1.24,6.46,6.46,0,0,0-.29.67,8.18,8.18,0,0,0-.28,2.1v329.7a8,8,0,0,0,4,6.95l192.5,110.84a8.83,8.83,0,0,0,1.33.54c.21.08.41.2.63.26a7.92,7.92,0,0,0,4.1,0c.2-.05.37-.16.55-.22a8.6,8.6,0,0,0,1.4-.58L404.4,400.09a8,8,0,0,0,4-6.95V287.88l92.24-53.11a8,8,0,0,0,4-7V117.92A8.63,8.63,0,0,0,504.4,115.83ZM111.6,17.28h0l80.19,46.15-80.2,46.18L31.41,63.44Zm88.25,60V278.6l-46.53,26.79-33.69,19.4V123.5l46.53-26.79Zm0,412.78L23.37,388.5V77.32L57.06,96.7l46.52,26.8V338.68a6.94,6.94,0,0,0,.12.9,8,8,0,0,0,.16,1.18h0a5.92,5.92,0,0,0,.38.9,6.38,6.38,0,0,0,.42,1v0a8.54,8.54,0,0,0,.6.78,7.62,7.62,0,0,0,.66.84l0,0c.23.22.52.38.77.58a8.93,8.93,0,0,0,.86.66l0,0,0,0,92.19,52.18Zm8-106.17-80.06-45.32,84.09-48.41,92.26-53.11,80.13,46.13-58.8,33.56Zm184.52,4.57L215.88,490.11V397.8L346.6,323.2l45.77-26.15Zm0-119.13L358.68,250l-46.53-26.79V131.79l33.69,19.4L392.37,178Zm8-105.28-80.2-46.17,80.2-46.16,80.18,46.15Zm8,105.28V178L455,151.19l33.68-19.4v91.39h0Z"/>
+                            </svg>
+                            Laravel
+                        </x-button-link>
+                        <x-button-link
+                            href="https://alpinejs.dev/start-here"
+                            target="_blank"
+                            class="mr-3 my-1 bg-white border-primary text-[#2D3441] font-bold hover:bg-opacity-80"
+                        >
+                            <svg fill="none" height="22px" class="fill-current inline-block mb-1" viewBox="1.069 54 198.26 91.221" width="22px" xmlns="http://www.w3.org/2000/svg">
+                                <g clip-rule="evenodd" fill-rule="evenodd">
+                                    <path d="m155.271 54 44.058 44.002-44.058 44.001-44.057-44.001z" fill="#77c1d2"/>
+                                    <path d="m45.127 54 91.337 91.221h-88.115l-47.28-47.22z" fill="#2d3441"/>
+                                </g>
+                            </svg>
+                            Alpine.js
+                        </x-button-link>
+                        <x-button-link
+                            href="https://tailwindcss.com/docs/guides/laravel"
+                            target="_blank"
+                            variant="dark" class="mr-3 my-1 font-bold bg-slate-800 hover:bg-opacity-80" target="_blank"
+                        >
+                            <svg fill="none" height="22px" class="fill-current inline-block mb-1" viewBox="0 0 24 24" width="22px" xmlns="http://www.w3.org/2000/svg">
+                                <path fill="#38bdf8" d="M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 C13.666,10.618,15.027,12,18.001,12c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 c1.177,1.194,2.538,2.576,5.512,2.576c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C10.337,13.382,8.976,12,6.001,12z"/>
+                            </svg>
+                            Tailwind.css
+                        </x-button-link>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <span class="absolute top-0 left-0 z-[-1]">
+                    <svg
+                        width="189"
+                        height="162"
+                        viewBox="0 0 189 162"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <ellipse
+                            cx="16"
+                            cy="-16.5"
+                            rx="173"
+                            ry="178.5"
+                            transform="rotate(180 16 -16.5)"
+                            fill="url(#paint0_linear)"
+                        />
+                        <defs>
+                        <linearGradient
+                            id="paint0_linear"
+                            x1="-157"
+                            y1="-107.754"
+                            x2="98.5011"
+                            y2="-106.425"
+                            gradientUnits="userSpaceOnUse"
+                        >
+                            <stop stop-color="white" stop-opacity="0.07" />
+                            <stop offset="1" stop-color="white" stop-opacity="0" />
+                        </linearGradient>
+                        </defs>
+                    </svg>
+                </span>
+                <span class="absolute bottom-0 right-0 z-[-1]">
+                    <svg
+                        width="191"
+                        height="208"
+                        viewBox="0 0 191 208"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <ellipse
+                            cx="173"
+                            cy="178.5"
+                            rx="173"
+                            ry="178.5"
+                            fill="url(#paint0_linear)"
+                        />
+                        <defs>
+                            <linearGradient
+                                id="paint0_linear"
+                                x1="-3.27832e-05"
+                                y1="87.2457"
+                                x2="255.501"
+                                y2="88.5747"
+                                gradientUnits="userSpaceOnUse"
+                            >
+                                <stop stop-color="white" stop-opacity="0.07" />
+                                <stop offset="1" stop-color="white" stop-opacity="0" />
+                            </linearGradient>
+                        </defs>
+                    </svg>
+                </span>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ====== Call To Action Section End -->
